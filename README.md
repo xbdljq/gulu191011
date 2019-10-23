f2763ce8bf04051e3b03aeb8b098bc2b1949d2bd
2044527225@qq.com <jqliang@jqdeMacBook-Pro.local>
2019-10-23 17:27:28 +0800

    单元测试chai.js与mock

--- README.md
+++ README.md
@@ -18,8 +18,30 @@
 
 
  ## 过程
+ - 设置淘宝源  npm install -g cnpm --registry=https://registry.npm.taobao.org
+ - 设置淘宝源 sudo npm config set registry https://registry.npm.taobao.org/
+ - 安装node_sass     SASS_BINARY_SITE=https://npm.taobao.org/mirrors/node-sass/ npm install node-sass
  - 安装打包工具parcel    npm install -g parcel-bundler
  - 运行   ./node_modules/.bin/parcel index.html --no-cache
  
  - 工具 
  - npm i -g git-open  安装后可直接用git open命令打开git提交页面
+
+ ### 单元测试chai.js与mock
+ > BDD---Behavior Driven Development  行为驱动开发
+ > TDD---Test-Driven Development      测试驱动开发
+ > Assert --  断言  console.assert()
+
+ - 安装chai   npm i -D chai
+ - npm i -D chai-spies
+
+ ### 1、自动化测试
+ - 使用 Karma + Mocha做单元测试
+
+### 2、持续集成
+
+### 3、重写所有的代码
+
+### 4、发布npm包
+
+### 5、完善README.MD
--- src/app.js
+++ src/app.js
@@ -21,141 +21,141 @@ new Vue({
 })
 
 //单元测试
-// import chai from 'chai'
-// import spies from 'chai-spies'
-// chai.use(spies)
-// const expect = chai.expect
-// try {
-//     {
-//         const Constructor = Vue.extend(Button)
-//         const button = new Constructor({
-//                 propsData: {
-//                     icon: 'settings'
-//                 }
-//             })
-//             //button.$mount('#test')
-//         button.$mount()
-//         let useElement = button.$el.querySelector('use')
-//         let href = useElement.getAttribute('xlink:href')
-//         expect(href).to.eq('#i-settings')
-//         button.$el.remove()
-//         button.$destroy()
-//     } {
-//         const Constructor = Vue.extend(Button)
-//         const button = new Constructor({
-//                 propsData: {
-//                     icon: 'settings',
-//                     loading: true
-//                 }
-//             })
-//             //button.$mount('#test')
-//         button.$mount()
-//         let useElement = button.$el.querySelector('use')
-//         let href = useElement.getAttribute('xlink:href')
-//         expect(href).to.eq('#i-loading')
-//         button.$el.remove()
-//         button.$destroy()
-//     }
+import chai from 'chai'
+import spies from 'chai-spies'
+chai.use(spies)
+const expect = chai.expect
+try {
+    {
+        const Constructor = Vue.extend(Button)
+        const button = new Constructor({
+                propsData: {
+                    icon: 'settings'
+                }
+            })
+            //button.$mount('#test')
+        button.$mount()
+        let useElement = button.$el.querySelector('use')
+        let href = useElement.getAttribute('xlink:href')
+        expect(href).to.eq('#i-settings')
+        button.$el.remove()
+        button.$destroy()
+    } {
+        const Constructor = Vue.extend(Button)
+        const button = new Constructor({
+                propsData: {
+                    icon: 'settings',
+                    loading: true
+                }
+            })
+            //button.$mount('#test')
+        button.$mount()
+        let useElement = button.$el.querySelector('use')
+        let href = useElement.getAttribute('xlink:href')
+        expect(href).to.eq('#i-loading')
+        button.$el.remove()
+        button.$destroy()
+    }
 
-//     {
-//         const Constructor = Vue.extend(Button)
-//         const button = new Constructor({
-//                 propsData: {
-//                     icon: 'settings',
-//                     loading: true
-//                 }
-//             })
-//             //button.$mount('#test')
-//         button.$mount()
-//         let useElement = button.$el.querySelector('use')
-//         let href = useElement.getAttribute('xlink:href')
-//         expect(href).to.eq('#i-loading')
-//         button.$el.remove()
-//         button.$destroy()
-//     } {
-//         const div = document.createElement('div')
-//         document.body.appendChild(div)
-//         const Constructor = Vue.extend(Button)
-//         const button = new Constructor({
-//                 propsData: {
-//                     icon: 'settings'
-//                 }
-//             })
-//             //button.$mount('#test')
-//         button.$mount(div)
-//         let svg = button.$el.querySelector('svg')
-//         let { order } = window.getComputedStyle(svg)
-//         expect(order).to.eq('1')
+    {
+        const Constructor = Vue.extend(Button)
+        const button = new Constructor({
+                propsData: {
+                    icon: 'settings',
+                    loading: true
+                }
+            })
+            //button.$mount('#test')
+        button.$mount()
+        let useElement = button.$el.querySelector('use')
+        let href = useElement.getAttribute('xlink:href')
+        expect(href).to.eq('#i-loading')
+        button.$el.remove()
+        button.$destroy()
+    } {
+        const div = document.createElement('div')
+        document.body.appendChild(div)
+        const Constructor = Vue.extend(Button)
+        const button = new Constructor({
+                propsData: {
+                    icon: 'settings'
+                }
+            })
+            //button.$mount('#test')
+        button.$mount(div)
+        let svg = button.$el.querySelector('svg')
+        let { order } = window.getComputedStyle(svg)
+        expect(order).to.eq('1')
 
-//         button.$el.remove()
-//         button.$destroy()
-//     }
+        button.$el.remove()
+        button.$destroy()
+    }
 
-//     {
-//         const div = document.createElement('div')
-//         document.body.appendChild(div)
-//         const Constructor = Vue.extend(Button)
-//         const button = new Constructor({
-//                 propsData: {
-//                     icon: 'settings',
-//                     iconPosition: 'right'
-//                 }
-//             })
-//             //button.$mount('#test')
-//         button.$mount(div)
-//         let svg = button.$el.querySelector('svg')
-//         let { order } = window.getComputedStyle(svg)
-//         expect(order).to.eq('2')
+    {
+        const div = document.createElement('div')
+        document.body.appendChild(div)
+        const Constructor = Vue.extend(Button)
+        const button = new Constructor({
+                propsData: {
+                    icon: 'settings',
+                    iconPosition: 'right'
+                }
+            })
+            //button.$mount('#test')
+        button.$mount(div)
+        let svg = button.$el.querySelector('svg')
+        let { order } = window.getComputedStyle(svg)
+        expect(order).to.eq('2')
 
-//         button.$el.remove()
-//         button.$destroy()
-//     }
+        button.$el.remove()
+        button.$destroy()
+    }
 
-//     {
-//         const div = document.createElement('div')
-//         document.body.appendChild(div)
-//         const Constructor = Vue.extend(Button)
-//         const button = new Constructor({
-//                 propsData: {
-//                     icon: 'settings',
-//                     iconPosition: 'right'
-//                 }
-//             })
-//             //button.$mount('#test')
-//         button.$mount(div)
-//         let svg = button.$el.querySelector('svg')
-//         let { order } = window.getComputedStyle(svg)
-//         expect(order).to.eq('2')
+    {
+        const div = document.createElement('div')
+        document.body.appendChild(div)
+        const Constructor = Vue.extend(Button)
+        const button = new Constructor({
+                propsData: {
+                    icon: 'settings',
+                    iconPosition: 'right'
+                }
+            })
+            //button.$mount('#test')
+        button.$mount(div)
+        let svg = button.$el.querySelector('svg')
+        let { order } = window.getComputedStyle(svg)
+        expect(order).to.eq('2')
 
-//         button.$el.remove()
-//         button.$destroy()
-//     } {
-//         //chai-spies  --- https://github.com/chaijs/chai-spies
-//         //mock
-//         const Constructor = Vue.extend(Button)
-//         const vm = new Constructor({
-//                 propsData: {
-//                     icon: 'settings',
-//                 }
-//             })
-//             //button.$mount('#test')
-//         vm.$mount()
+        button.$el.remove()
+        button.$destroy()
+    } {
+        //chai-spies  --- https://github.com/chaijs/chai-spies
+        //mock
+        const Constructor = Vue.extend(Button)
+        const vm = new Constructor({
+                propsData: {
+                    icon: 'settings',
+                }
+            })
+            //button.$mount('#test')
+        vm.$mount()
 
-//         let spy = chai.spy(function() {})
+        let spy = chai.spy(function() {})
 
-//         vm.$on('click', spy)
-//             //希望这个函数被执行
-//         let button = vm.$el
-//         button.click()
-//         expect(spy).to.have.been.called()
+        vm.$on('click', spy)
+            //希望这个函数被执行
+        let button = vm.$el
+        button.click()
+        expect(spy).to.have.been.called()
 
-//         // button.$el.remove()
-//         // button.$destroy()
-//     }
-// } catch (error) {
-//     window.errors = [error]
-// } finally {
-//     window.error && window.errors.forEach((error) => {
-//         console.error(error.message)
-//     })
-// }
+        // button.$el.remove()
+        // button.$destroy()
+    }
+} catch (error) {
+    window.errors = [error]
+} finally {
+    window.error && window.errors.forEach((error) => {
+        console.error(error.message)
+    })
+}
